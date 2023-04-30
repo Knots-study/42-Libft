518745107c3011f595abcd9a496ba214a1b0c8ba
Knots-study <y.karaki.732@ms.saitama-u.ac.jp>
2023-05-01 05:08:46 +0900

    add ft

---
+++ libft.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: knottey <Twitter:@knottey>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/01 04:42:09 by knottey           #+#    #+#             */
+/*   Updated: 2023/05/01 04:58:49 by knottey          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int ft_islower(int c);
+int ft_isupper(int c);
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
