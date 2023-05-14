cd481d89cca4739b82c41343ae44b8a7d63d324b
Knots <y.karaki.732@ms.saitama-u.ac.jp>
2023-05-14 23:06:41 +0900

    fix calloc and strnstr

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: knottey <Twitter:@knottey>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 21:06:59 by knottey           #+#    #+#             */
-/*   Updated: 2023/05/14 09:05:53 by knottey          ###   ########.fr       */
+/*   Updated: 2023/05/14 09:17:03 by knottey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,13 @@ void	*ft_calloc(size_t count, size_t size)
 	if (size != 0 && count > SIZE_MAX / size)
 		return (NULL);
 	if (count == 0 || size == 0)
-		memory = (void *)malloc(1);
-	else
-		memory = (void *)malloc(count * size);
+	{
+		count = 1;
+		size = 1;
+	}
+	memory = (void *)malloc(count * size);
 	if (memory == NULL)
 		return (NULL);
-	ft_bzero(memory, size * count);
+	ft_bzero(memory, count * size);
 	return (memory);
 }
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -14,9 +14,22 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	char *u= "aiueo";
-	(void)haystack;
-	(void)needle;
-	(void)len;
-	return (u);
+	size_t	i;
+	size_t	j;
+
+	if (haystack == NULL || needle == NULL)
+		return (NULL);
+	i = 0;
+	if (needle[0] == '\0')
+		return ((char *)haystack);
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j] && haystack[i + j] != '\0' && i + j < len)
+			j++;
+		if (j == ft_strlen(needle))
+			return ((char *)&haystack[i]);
+		i++;
+	}
+	return (NULL);
 }
