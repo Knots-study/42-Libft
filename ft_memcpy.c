960af78f0ca8942cb9b137d50329426987c18a57
Knots <y.karaki.732@ms.saitama-u.ac.jp>
2023-05-16 22:56:09 +0900

    norminette pass

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,24 +6,24 @@
 /*   By: knottey <Twitter:@knottey>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 12:45:53 by knottey           #+#    #+#             */
-/*   Updated: 2023/05/16 22:52:43 by knottey          ###   ########.fr       */
+/*   Updated: 2023/05/16 22:55:52 by knottey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    *ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-    char    *c_dest;
-    char    *c_src;
+	char	*c_dest;
+	char	*c_src;
 
-    c_dest = (char *)dest;
-    c_src = (char *)src;
-    while (n--)
-    {
-        *c_dest = *c_src;
-        c_dest++;
-        c_src++;
-    }
-    return (dest);
+	c_dest = (char *)dest;
+	c_src = (char *)src;
+	while (n--)
+	{
+		*c_dest = *c_src;
+		c_dest++;
+		c_src++;
+	}
+	return (dest);
 }
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,25 +6,25 @@
 /*   By: knottey <Twitter:@knottey>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 12:42:58 by knottey           #+#    #+#             */
-/*   Updated: 2023/05/16 22:51:15 by knottey          ###   ########.fr       */
+/*   Updated: 2023/05/16 22:55:47 by knottey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    *ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-    char *u_dest;
-    char *u_src;
+	char *u_dest;
+	char *u_src;
 
-    u_dest = (char *)dest;
-    u_src = (char *)src;
-    if (dest < src)
-        ft_memcpy(dest, src, n);
-    else
-    {
-        while (n--)
-            *(u_dest + n) = *(u_src + n);
-    }
-    return (dest);
+	u_dest = (char *)dest;
+	u_src = (char *)src;
+	if (dest < src)
+		ft_memcpy(dest, src, n);
+	else
+	{
+		while (n--)
+			*(u_dest + n) = *(u_src + n);
+	}
+	return (dest);
 }
