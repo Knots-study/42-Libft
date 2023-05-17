e3f96462193d5f2fb799d7a06d278d93c4a993d8
Knots <y.karaki.732@ms.saitama-u.ac.jp>
2023-05-17 16:59:52 +0900

    [update] ft_atoi.c : 動くように機能追加(オーバーフロ―対策も) [fix] ft_atoi.c : norminetteエラー修正

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: knottey <Twitter:@knottey>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 21:22:35 by knottey           #+#    #+#             */
-/*   Updated: 2023/05/15 07:21:12 by knottey          ###   ########.fr       */
+/*   Updated: 2023/05/17 16:59:40 by knottey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,15 @@
 
 int	ft_isspace(int c)
 {
-	c = (unsigned char)c;
-	return (c == '\t' || c == '\n' || c == '\v' ||
-		c == '\f' || c == '\r'	|| c == ' ');
+	return (c == '\t' || c == '\n' || c == '\v'
+		|| c == '\f' || c == '\r' || c == ' ');
 }
 
 int	ft_space_sign(const char *str, const char **endptr)
 {
 	int	sign;
 
-	while (ft_isspace((unsigned char)*str))
+	while (ft_isspace(*str))
 		str++;
 	sign = 1;
 	if (*str == '-')
@@ -38,6 +37,14 @@ int	ft_space_sign(const char *str, const char **endptr)
 	return (sign);
 }
 
+int	over_num(int sign)
+{
+	if (sign == 1)
+		return ((int)(LONG_MAX));
+	else
+		return ((int)(LONG_MIN));
+}
+
 int	ft_atoi(const char *str)
 {
 	int				sign;
@@ -45,11 +52,15 @@ int	ft_atoi(const char *str)
 
 	sign = ft_space_sign(str, &str);
 	ans = 0;
-	while ('0' <= *str && *str <= '9')
+	while (ft_isdigit(*str))
 	{
+		if (ans > LONG_MAX / 10)
+			return (over_num(sign));
+		if (ans == LONG_MAX / 10 && (*str - '0') > LONG_MAX % 10)
+			return (over_num(sign));
 		ans *= 10;
 		ans += *str - '0';
 		str++;
 	}
-	return (ans * sign);
+	return ((int)(ans * sign));
 }
