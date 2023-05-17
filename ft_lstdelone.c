dec163b1be2c7e92b0df9d5fccd44b16cec3f5aa
Knots <y.karaki.732@ms.saitama-u.ac.jp>
2023-05-17 14:27:27 +0900

    [fix] ft_lstdelone.c : norminetteエラーの解消

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: knottey <Twitter:@knottey>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 00:54:13 by knottey           #+#    #+#             */
-/*   Updated: 2023/05/17 08:46:51 by knottey          ###   ########.fr       */
+/*   Updated: 2023/05/17 14:27:14 by knottey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	if	(lst == NULL || del == NULL)
+	if (lst == NULL || del == NULL)
 		return ;
 	(*del)(lst->content);
 	free(lst);
