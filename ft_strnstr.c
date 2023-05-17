a37685b18c12e84a067c267957dc5d31c72a5aaa
Knots <y.karaki.732@ms.saitama-u.ac.jp>
2023-05-17 14:45:08 +0900

    [update] ft_strnstr.c : アルゴリズムの改良 [fix] ft_strnstr.c : nominetteエラーの解消

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -14,22 +14,20 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	i;
-	size_t	j;
+	size_t	needle_len;
 
 	if (haystack == NULL || needle == NULL)
 		return (NULL);
-	i = 0;
 	if (needle[0] == '\0')
 		return ((char *)haystack);
-	while (haystack[i] != '\0')
+	i = 0;
+	needle_len = ft_strlen(needle);
+	while (*haystack != '\0' && len >= needle_len)
 	{
-		j = 0;
-		while (haystack[i + j] == needle[j] && haystack[i + j] != '\0' && i + j < len)
-			j++;
-		if (j == ft_strlen(needle))
-			return ((char *)&haystack[i]);
-		i++;
+		if (ft_strncmp(haystack, needle, needle_len) == 0)
+			return ((char *)haystack);
+		haystack++;
+		len--;
 	}
 	return (NULL);
 }
