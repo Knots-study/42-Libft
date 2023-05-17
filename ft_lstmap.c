1995ce28c9c9f3dcf25a6acc9528d2c4020699a1
Knots <y.karaki.732@ms.saitama-u.ac.jp>
2023-05-17 14:25:53 +0900

    [update] ft_lstmap.cの機能を追加しました

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: knottey <Twitter:@knottey>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 00:56:12 by knottey           #+#    #+#             */
-/*   Updated: 2023/05/11 00:55:55 by knottey          ###   ########.fr       */
+/*   Updated: 2023/05/17 14:24:15 by knottey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,26 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	(void)lst;
-	(void)f;
-	(void)del;
-	return (NULL);
+	t_list	*head;
+	t_list	*new_elem;
+	t_list	*tail;
+
+	if (lst == NULL || f == NULL)
+		return (NULL);
+	head = ft_lstnew((*f)(lst->content));
+	tail = head;
+	lst = lst->next;
+	while (lst != NULL)
+	{
+		new_elem = ft_lstnew((*f)(lst->content));
+		if (new_elem == NULL)
+		{
+			ft_lstclear(&head, del);
+			return (NULL);
+		}
+		tail->next = new_elem;
+		tail = tail->next;
+		lst = lst->next;
+	}
+	return (head);
 }
