2457e241d2b3275f95287293423532e3f6a4c540
Knots <y.karaki.732@ms.saitama-u.ac.jp>
2023-05-18 11:48:45 +0900

    [fix] ft_strmapi.c: 引数がNULLだった時のエラー処理を追加

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: knottey <Twitter:@knottey>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 01:00:27 by knottey           #+#    #+#             */
-/*   Updated: 2023/05/11 23:05:02 by knottey          ###   ########.fr       */
+/*   Updated: 2023/05/18 11:47:40 by knottey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,17 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
+	char	*str;
 	size_t	idx;
 	size_t	word_length;
-	char	*str;
 
-	idx = 0;
+	if (s == NULL || f == NULL)
+		return (NULL);
 	word_length = ft_strlen(s);
 	str = (char *)malloc(sizeof(char) * (word_length + 1));
 	if (str == NULL)
 		return (NULL);
+	idx = 0;
 	while (idx < word_length)
 	{
 		str[idx] = f(idx, s[idx]);
