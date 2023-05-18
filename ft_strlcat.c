ebb998534e700e208c3eca89ea59fb45830efb64
Knots <y.karaki.732@ms.saitama-u.ac.jp>
2023-05-18 12:16:25 +0900

    [update] ft_strlcat.c : dstsize=0の時の処理を変更

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -18,7 +18,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	src_length;
 
 	src_length = ft_strlen(src);
-	if (dst == NULL)
+	if (dstsize == 0)
 		return (src_length);
 	dst_length = ft_strlen(dst);
 	if (dstsize <= dst_length)
