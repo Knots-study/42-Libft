52a8063a7e16cec828abee8430c69e01809bdc8f
Knots <y.karaki.732@ms.saitama-u.ac.jp>
2023-05-18 11:26:36 +0900

    [clean] ft_strjoin.c : 代入順番を変更

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: knottey <Twitter:@knottey>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 01:01:58 by knottey           #+#    #+#             */
-/*   Updated: 2023/05/18 11:19:48 by knottey          ###   ########.fr       */
+/*   Updated: 2023/05/18 11:26:28 by knottey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	s2_len;
 	char	*join_word;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
 	s1_len = 0;
 	s2_len = 0;
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
 	if (s1 != NULL)
 		s1_len = ft_strlen(s1);
 	if (s2 != NULL)
